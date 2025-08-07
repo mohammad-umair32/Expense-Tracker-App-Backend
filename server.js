5b7988622c21cb1771d405cc2ed13de11c4e0dab
mohammad-umair32 <mohammadumair.720855@gmail.com>
2025-08-07 17:10:49 +0530

    add delete endpoint

--- server.js
+++ server.js
@@ -67,6 +67,30 @@ app.post("/api/transactions", async (req, res) => {
   }
 });
 
+app.delete("/api/transactions/:id", async (req,res) => {
+  try {
+    const { id } = req.params;
+
+    if(isNaN(parseInt(id))){
+      return res.status(400).json({message:"Invalid transactions ID"})
+    }
+
+    const result = await sql`
+    DELETE FROM transactions WHERE id = ${id} RETURNING *
+    `;
+
+    if(result.length === 0){
+      return res.status(404).json({message:"Transactions not found"})
+    }
+
+    res.status(200).json({message:"Transaction deleted successfully"});
+
+  } catch (error) {
+    console.log("Error deleting the transatons", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+})
+
 initDB().then(() => {
   app.listen(PORT, () => {
     console.log("Server is up and running on PORT:", PORT);
