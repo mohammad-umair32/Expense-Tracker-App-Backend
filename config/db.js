ca19fae17e61c6ecc01c804ed33be17dae8e4034
mohammad-umair32 <mohammadumair.720855@gmail.com>
2025-08-07 18:28:34 +0530

    final commit

--- config/db.js
+++
@@ -1,6 +0,0 @@
-import {neon} from '@neondatabase/serverless'
-
-import "dotenv/config";
-
-//Creates sql connection using our DB URL
-export const sql = neon(process.env.DATABASE_URL) 
---
+++ src/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import { initDB } from "./config/db.js";
+import ratelimiter from "./middleware/rateLimiter.js";
+
+import transactionsRoute from "./routes/transactionsRoute.js"
+
+dotenv.config();
+const app = express();
+
+//middleware
+app.use(ratelimiter)
+app.use(express.json());
+
+const PORT = process.env.PORT || 5001;
+
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+app.use("/api/transactions", transactionsRoute);
+
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is up and running on PORT:", PORT);
+  });
+});
